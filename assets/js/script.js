1586318da12d3050d8e0f40b8159dc374c6b5608
Shirly <shirlyd@axioned.com>
2021-09-26 21:17:05 +0530

    js function initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
-/* Author: 
+let saveBtn = document.querySelector('.save');
+let itemText = document.querySelector('.item');
+let quantityText = document.querySelector('.quantity');
 
-*/
+saveBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(itemText.value == "" || quantityText.value == "") {
+        return alert('Please fill all fields');
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let item = localStorage.getItem("item");
+    if(item == null) {
+        itemObject = [];
+    } else {
+        itemObject = JSON.parse(item);
+    }
+    let myObj = {
+        itemTitle: itemText.value,
+        itemQuantity: quantityText.value
+    }
+    itemObject.push(myObj);
+    localStorage.setItem("item", JSON.stringify(itemObject));
+    itemText.value = "";
+    quantityText.value = "";
+})
